b50d297a9649481700f0a3e22519512e8b3bc9f6
Josh Quintana <josh.d.quintana@gmail.com>
2015-10-22 21:08:02 -0400

    Add npm scripts

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import compression from 'compression';
 
 const app = new express();
 app
+	.use( compression() )
 	.use( '/', express.static( 'public' ) )
 	.use( '/', express.static( 'build' ) )
 	.use( '/', express.static( '.' ) )
